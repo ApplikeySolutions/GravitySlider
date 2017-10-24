69ae8b04ed1d1b89cfd0c17381e081a10826aaa9
Ivan Hahanov <ivan.hahanov@applikeysolutions.com>
2017-10-24 12:21:07 +0300

    Initial commit

---
+++ Example/Pods/Target Support Files/GravitySlider/GravitySlider-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GravitySlider : NSObject
+@end
+@implementation PodsDummy_GravitySlider
+@end
---
+++ Example/Pods/Target Support Files/Pods-GravitySlider_Tests/Pods-GravitySlider_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GravitySlider_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_GravitySlider_Tests
+@end

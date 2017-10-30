ce79090a1f7ccbe4567c18f83017015b5276fd13
Ivan Hahanov <ivan.hahanov@applikeysolutions.com>
2017-10-30 10:37:03 +0200

    remove wrong files

--- Example/Pods/Target Support Files/GravitySlider/GravitySlider-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GravitySlider : NSObject
-@end
-@implementation PodsDummy_GravitySlider
-@end
--- Example/Pods/Target Support Files/Pods-GravitySlider_Tests/Pods-GravitySlider_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GravitySlider_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_GravitySlider_Tests
-@end
